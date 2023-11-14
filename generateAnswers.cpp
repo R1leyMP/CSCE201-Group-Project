433217fe30a1075ba9cb795c8eec512895350c62
R1leyMP <148601021+R1leyMP@users.noreply.github.com>
2023-11-14 13:04:17 -0900

    Update generateAnswers.cpp

--- generateAnswers.cpp
+++ generateAnswers.cpp
@@ -71,7 +71,7 @@ char generateAnswers(vector<string> a_list, int a_index, vector<string> q_list, 
                 j = rand() % a_list.size();
                 if (j == a_index){
                     while (j == a_index){
-                        J = rand() % a_list.size();
+                        j = rand() % a_list.size();
                     }
                 }
             } while (findNum(j, options));
